03730328a45b051dbba127476c15e41069b3ac60
leofriskey <leofriskey@pm.me>
2024-04-12 04:38:22 +0500

    v 0.1

---
+++ Sources/ConstraintsHolder/Constraints.swift
@@ -0,0 +1,124 @@
+import UIKit
+
+/// Contains all constrains references inside `storage`
+final class Constraints {
+    private init() { }
+    /// Constraints singleton instance
+    static private let main = Constraints()
+    /// Constraints holders stored by <UIVIewID>
+    private var storage: [UIViewID: ConstraintsHolder] = [:]
+}
+
+extension Constraints {
+    /// Layout anchor type
+    enum ConstraintType: String {
+        /// - - -
+        case left, right, top, bottom, leading, trailing, centerX, centerY, width, height, baseline
+        /// - - -
+        ///
+        ///
+        ///
+        /// - - -
+        init?(attribute: NSLayoutConstraint.Attribute) {
+            var rawValue = ""
+            
+            switch attribute {
+            case .left:
+                rawValue = Self.left.rawValue
+            case .right:
+                rawValue = Self.right.rawValue
+            case .top:
+                rawValue = Self.top.rawValue
+            case .bottom:
+                rawValue = Self.bottom.rawValue
+            case .leading:
+                rawValue = Self.leading.rawValue
+            case .trailing:
+                rawValue = Self.trailing.rawValue
+            case .width:
+                rawValue = Self.width.rawValue
+            case .height:
+                rawValue = Self.height.rawValue
+            case .centerX:
+                rawValue = Self.centerX.rawValue
+            case .centerY:
+                rawValue = Self.centerY.rawValue
+            case .lastBaseline, .firstBaseline:
+                rawValue = Self.baseline.rawValue
+            default:
+                ()
+            }
+            self.init(rawValue: rawValue)
+        }
+        /// - - -
+    }
+}
+
+
+extension Constraints {
+    /// Exposes  constraints container that is bound to specified view
+    static func updateConstraints(_ view: UIView, exposeHolder: (ConstraintsHolder) -> Void) {
+        let viewID = view.getViewID()
+        
+        if let constraintHolder = main.storage[viewID] {
+            exposeHolder(constraintHolder)
+        } else {
+            let constraintHolder = ConstraintsHolder()
+            main.storage[viewID] = constraintHolder
+            exposeHolder(constraintHolder)
+        }
+    }
+    
+    /// Clears up constraints container that is bound to specified view
+    static func removeAllConstraints(_ view: UIView) {
+        let viewID = view.getViewID()
+        
+        if let holder = main.storage[viewID] {
+            holder.all.forEach {
+                guard $0.isActive == false else {
+                    let constraintType: String = holder.findTypeOfConstraint($0)?.rawValue ?? ""
+                    fatalError("\(constraintType) constraint: \n \($0) must be deactivated before being removed from container.")
+                }
+            }
+        }
+        
+        main.storage[viewID] = nil
+    }
+}
+
+
+// MARK: - UIView
+// Convinience methods
+extension UIView {
+    /// Exposes  constraints container that is bound to this view
+    func updateConstraints(exposeHolder: (Constraints.ConstraintsHolder) -> Void) {
+        Constraints.updateConstraints(self) {
+            exposeHolder($0)
+        }
+    }
+    
+    func removeFromeSuperViewAndClearConstraints() {
+        self.removeAllConstraints()
+        self.removeFromSuperview()
+    }
+    
+    /// Clears up constraints container that is bound to this view
+    func removeAllConstraints() {
+        Constraints.removeAllConstraints(self)
+    }
+    
+    /// Retrievs view's id
+    func getViewID() -> UIViewID {
+        let viewID: UIViewID
+        
+        if let id = self.accessibilityIdentifier {
+            viewID = id
+        } else {
+            let id = UUID().uuidString
+            self.accessibilityIdentifier = id
+            viewID = id
+        }
+        
+        return viewID
+    }
+}
--- Sources/ConstraintsHolder/ConstraintsHolder.swift
+++ Sources/ConstraintsHolder/ConstraintsHolder.swift
@@ -1,2 +1,222 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+extension Constraints {
+    /// Container assigned to view with all constraints that has been added to it
+    class ConstraintsHolder {
+        /// constraint values with a constraint-type as a key
+        private var constraints: [ConstraintType: NSLayoutConstraint?] = [:]
+        
+        /// - - -
+        var left: NSLayoutConstraint? {
+            get {
+                constraints[.left] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .left
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var right: NSLayoutConstraint? {
+            get {
+                constraints[.right] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .right
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var top: NSLayoutConstraint? {
+            get {
+                constraints[.top] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .top
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var bottom: NSLayoutConstraint? {
+            get {
+                constraints[.bottom] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .bottom
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var leading: NSLayoutConstraint? {
+            get {
+                constraints[.leading] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .leading
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var trailing: NSLayoutConstraint? {
+            get {
+                constraints[.trailing] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .trailing
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var centerX: NSLayoutConstraint? {
+            get {
+                constraints[.centerX] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .centerX
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var centerY: NSLayoutConstraint? {
+            get {
+                constraints[.centerY] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .centerY
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var baseline: NSLayoutConstraint? {
+            get {
+                constraints[.baseline] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .baseline
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var width: NSLayoutConstraint? {
+            get {
+                constraints[.width] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .width
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        
+        var height: NSLayoutConstraint? {
+            get {
+                constraints[.height] ?? nil
+            }
+            set {
+                // type of constraint that is valid for this variable
+                let constraintVariableType: ConstraintType = .height
+                
+                verifyConstraintAndAssignIt(newValue, constraintVariableType)
+            }
+        }
+        /// - - -
+        ///
+        ///
+        ///
+        /// - - -
+        /// Returns all constraints contained in this holder
+        var all: [NSLayoutConstraint] {
+            constraints.values.compactMap { $0 }
+        }
+        
+        /// Returns active constraints contained in this holder
+        func activeConstraints() -> [Constraints.ConstraintType: NSLayoutConstraint] {
+            var activeConstraints: [Constraints.ConstraintType: NSLayoutConstraint] = [:]
+            
+            constraints.forEach { key, value in
+                if value?.isActive == true {
+                    activeConstraints[key] = value
+                }
+            }
+            
+            return activeConstraints
+        }
+        /// - - -
+        ///
+        ///
+        ///
+        /// - - -
+        /// Activate specified constraints stored in holder
+        @MainActor func activate(_ keyPaths: [KeyPath<Constraints.ConstraintsHolder, NSLayoutConstraint?>]) {
+            let constraints = keyPaths.map { self[keyPath: $0] }
+            
+            let unwrappedConstraints = constraints.compactMap { $0 }
+            
+            guard unwrappedConstraints.count == keyPaths.count else {
+                fatalError("keyPath passed to activate() contained nil value constraint")
+            }
+            
+            NSLayoutConstraint.activate(unwrappedConstraints)
+        }
+        /// Deactivate specified constraints stored in holder
+        @MainActor func deactivate(_ keyPaths: [KeyPath<Constraints.ConstraintsHolder, NSLayoutConstraint?>]) {
+            let constraints = keyPaths.map { self[keyPath: $0] }
+            
+            let unwrappedConstraints = constraints.compactMap { $0 }
+            
+            guard unwrappedConstraints.count == constraints.count else {
+                fatalError("constraints passed to deactivate() contained nil value")
+            }
+            
+            NSLayoutConstraint.deactivate(unwrappedConstraints)
+        }
+        /// - - -
+        ///
+        ///
+        ///
+        /// - - -
+        func findTypeOfConstraint(_ constraint: NSLayoutConstraint) -> ConstraintType? {
+            constraints.first(where: { $0.value == constraint })?.key
+        }
+        /// - - -
+        ///
+        ///
+        ///
+        /// - - -
+        /// Perfom various checks before assigning newValue to `constraints`
+        private func verifyConstraintAndAssignIt(_ constraint: NSLayoutConstraint?, _ constraintVariableType: ConstraintType) {
+            // check that constraint is deactivated before removing from `constraints`
+            if constraint == nil && constraints[constraintVariableType]??.isActive == true {
+                fatalError("\(constraintVariableType) constraint must be deactivated first")
+            }
+            
+            // check that constraint is of valid type
+            if let constraint, let constraintType = ConstraintType(attribute: constraint.firstAttribute), constraintType != constraintVariableType {
+                fatalError("Can't assign value of \(ConstraintType.self) \(constraintType) to variable of \(ConstraintType.self) \(constraintVariableType)")
+            }
+            
+            // assign new value
+            constraints[constraintVariableType] = constraint
+        }
+        /// - - -
+    }
+}
